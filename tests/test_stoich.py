0b417ab7f3c72e437779a93a1bc966822563ef00
Matt Gebert <mattheuu.gebert@gmail.com>
2024-12-16 13:03:07 +1100

    test(test_stoich.py): Converted most tests to be decorated by pytest.mark.parameterise

--- tests/test_stoich.py
+++ tests/test_stoich.py
@@ -7,7 +7,7 @@ import numpy as np
 from kkcalc import stoichiometry as kk_stoich
 
 
-class basic_stoichiomety:
+class basic_stoichs:
     """Basic functions for creating and testing `stoichiometry` objects."""
 
     # Define chemical formulae for testing
@@ -18,114 +18,139 @@ class basic_stoichiomety:
     POLYMER_PMMA: str = "C5H8O2"
     """Poly(methyl methacrylate) (PMMA) chemical formula."""
 
-    def basic_PS(self) -> kk_stoich:
-        """
-        Creates a simple integer `stoichiometry` of polystyrene.
-        """
-        return kk_stoich(self.POLYMER_PS)
 
-    def basic_PMMA(self) -> kk_stoich:
-        """
-        Creates a simple integer `stoichiometry` of PMMA.
-        """
-        return kk_stoich(self.POLYMER_PMMA)
-
-    def basic_P3HT(self) -> kk_stoich:
-        """
-        Creates a simple integer `stoichiometry` of P3HT.
-        """
-        return kk_stoich(self.POLYMER_P3HT)
-
-
-class TestStoichiometryInit(basic_stoichiomety):
+class TestStoichiometryInit:
     """Tests for the creation of integer `stoichiometry` objects."""
 
-    def test_stiochiometry(self) -> None:
+    @pytest.mark.parametrize(
+        "compound",
+        [
+            basic_stoichs.POLYMER_PS,
+            basic_stoichs.POLYMER_P3HT,
+            basic_stoichs.POLYMER_PMMA,
+        ],
+    )
+    def test_stiochiometry(self, compound) -> None:
         """
         Tests the creation of a simple integer `stiochiometry`.
         """
-        obj = self.basic_PS()
-        assert str(obj) == "C8H8"
+        obj = kk_stoich(compound)
+        assert str(obj) == compound
 
 
-class fractional_stoichiometry(basic_stoichiomety):
+class fractional_stoichs(basic_stoichs):
     """Basic functions for creating and testing fractional `stoichiometry` objects."""
 
-    # Define fractional chemical formulae
-    """Poly(methyl methacrylate) (PMMA) chemical formula."""
     BLOCK_COPOLYMER_PS_PMMA: str = (
-        f"({basic_stoichiomety.POLYMER_PS})1.0({basic_stoichiomety.POLYMER_PMMA})1.2"
+        f"({basic_stoichs.POLYMER_PS})1.0({basic_stoichs.POLYMER_PMMA})1.2"
     )
-
-    def copolymer_PS_PMMA(self) -> kk_stoich:
-        """
-        Creates a fractional `stiochiometry`, combining Polystyrene and PMMA stoichiometries.
-        """
-        return kk_stoich(self.BLOCK_COPOLYMER_PS_PMMA)
+    """Block copolymer of PS and PMMA 1:1.2 chemical formula."""
 
 
-class TestStoichiometryFractional(fractional_stoichiometry):
+class TestStoichiometryFractional:
     """Tests for the creation of fractional `stoichiometry` objects."""
 
-    def test_copolymer(self) -> None:
+    @pytest.mark.parametrize(
+        "frac_str,compound",
+        [
+            (fractional_stoichs.BLOCK_COPOLYMER_PS_PMMA, "C14H17.6O2.4"),
+        ],
+    )
+    def test_copolymer(self, frac_str, compound) -> None:
         """
-        Tests the creation of a fractional `stoichiometry`.
+        Tests the creation of a fractional `stoichiometry`, where elements get grouped.
         """
-        obj = self.copolymer_PS_PMMA()
-        assert str(obj) == "C14H17.6O2.4"
+        obj = kk_stoich(frac_str)
+        assert str(obj) == compound
 
 
-class TestStoichometryOperations(fractional_stoichiometry):
+class TestStoichometryOperations:
     """Tests for the numerical and functional operators of `stoichiometry` objects."""
 
-    def test_stiochiometry_numeric(self) -> None:
+    @pytest.mark.parametrize(
+        "compound",
+        [
+            fractional_stoichs.POLYMER_PS,
+            fractional_stoichs.POLYMER_P3HT,
+            fractional_stoichs.POLYMER_PMMA,
+            fractional_stoichs.BLOCK_COPOLYMER_PS_PMMA,
+        ],
+    )
+    def test_stiochiometry_numeric(self, compound) -> None:
         """
         Tests the addition, multiplication and division of stoichiometry objects
         """
         # Create the integer components
-        pmma = self.basic_PMMA()
-        p3ht = self.basic_P3HT()
-        ps = self.basic_PS()
+        polymer = kk_stoich(compound)
 
-        # Create the block
-        block = self.copolymer_PS_PMMA()
-
-        # Test numeric operations
-        assert 0.5 * pmma == kk_stoich(f"({self.POLYMER_PMMA})0.5")
-        assert (1.0 * ps + 1.2 * pmma) == block
+        # Test multiplication operations
+        assert 0.5 * polymer == kk_stoich(f"({polymer})0.5")
 
         # Test division
-        assert p3ht / 2 == kk_stoich(f"({self.POLYMER_P3HT})0.5")
-        assert (p3ht // 2) == "C5H7"
+        assert polymer / 2 == kk_stoich(f"({polymer})0.5")
+        composition = polymer.composition
+        for i in range(len(composition)):
+            composition[i] = (composition[i][0], composition[i][1] / 2)
+        assert polymer / 2 == kk_stoich(composition)
 
-    def test_stoichiometry_equivalence(self) -> None:
+        # Test floor division
+        composition = polymer.composition
+        for i in range(len(composition)):
+            composition[i] = (composition[i][0], composition[i][1] // 2)
+        assert (polymer // 2) == kk_stoich(composition)
+
+    def test_block_numeric(self) -> None:
+        """
+        Tests the addition of copolymers to form a block
+        """
+        # Create the integer components
+        ps = kk_stoich(fractional_stoichs.POLYMER_PS)
+        pmma = kk_stoich(fractional_stoichs.POLYMER_PMMA)
+        # Create the block
+        block = kk_stoich(fractional_stoichs.BLOCK_COPOLYMER_PS_PMMA)
+        # Test numeric operations
+        assert (1.0 * ps + 1.2 * pmma) == block
+
+    @pytest.mark.parametrize(
+        "compound",
+        [
+            fractional_stoichs.POLYMER_PS,
+            fractional_stoichs.POLYMER_P3HT,
+            fractional_stoichs.POLYMER_PMMA,
+            fractional_stoichs.BLOCK_COPOLYMER_PS_PMMA,
+        ],
+    )
+    def test_stoichiometry_equivalence(self, compound) -> None:
         """
         Tests the equivalence of various forms of `stiochiometry` objects.
         """
         #
-        ps = self.basic_PS()
+        polymer = kk_stoich(compound)
 
         # Test equivalence operations
-        assert ps == "C8H8"
-        assert "C8H8" == ps
-        assert ps == ps.copy()
-        assert ps == 1.0 * ps
-        assert ps != "C9H9"
+        assert polymer == compound
+        assert compound == polymer  # reverse
+        assert polymer == polymer.copy()
+        assert polymer == 1.0 * polymer
+        # Check quantity of elements affects equivalence
+        assert polymer != kk_stoich(
+            [(elem[0], elem[1] * 1.1) for elem in polymer.composition]
+        )
 
-    def test_stoichiometry_len(self):
+    @pytest.mark.parametrize(
+        "compound,length",
+        [
+            (fractional_stoichs.POLYMER_PS, 16),
+            (fractional_stoichs.POLYMER_P3HT, 25),
+            (fractional_stoichs.POLYMER_PMMA, 15),
+            (fractional_stoichs.BLOCK_COPOLYMER_PS_PMMA, 33),
+        ],
+    )
+    def test_stoichiometry_len(self, compound, length) -> None:
         """
         Tests the atomic length of a `stoichiometry` object.
         """
-        # Create the integer components
-        pmma = self.basic_PMMA()
-        p3ht = self.basic_P3HT()
-        ps = self.basic_PS()
-
-        # Create the block
-        block = self.copolymer_PS_PMMA()
-
+        # Create the component
+        polymer = kk_stoich(compound)
         # Test length
-        assert len(pmma) == 15
-        assert len(p3ht) == 25
-        assert len(ps) == 16
-        assert len(block) == 33
+        assert len(polymer) == length
