7f10a538b9d96efe2d574227ab05838403fa1585
Matt Gebert <mattheuu.gebert@gmail.com>
2025-06-10 13:06:28 +1000

    fix(gui): GUI updates and pyproject changes to numpydoc file checking
    Uploading uncommitted changes

--- tests/test_transforms.py
+++ tests/test_transforms.py
@@ -1,4 +1,36 @@
+"""
+Tests for the `kk_transforms` module.
+
+This module tests basic functions that ought to match consistency of the Kramers-Kronig relations.
+The tested example functions are listed in `Hilbert Transforms` table from wikipedia:
+https://en.wikipedia.org/wiki/Hilbert_transform#Table_of_selected_Hilbert_transforms
+"""
+
 import pytest
+
+
+class TestKKTransforms:
+    """
+    Test the `kk_transforms` module.
+
+    This class contains tests for the Kramers-Kronig transforms.
+    """
+
+    class KK_Functions:
+        """
+        Test functions for the Kramers-Kronig transforms.
+
+        This class contains test functions for the Kramers-Kronig transforms.
+        """
+
+    def test_kk_transform(self):
+        """
+        Test the `kk_transforms.kk_transform` function.
+
+        Use a gaussian to test the simple kronig-kramers transform.
+        """
+        pass
+
 
 # class kk_transforms_tests():
 #     def test_improve_accuracy() -> None:
