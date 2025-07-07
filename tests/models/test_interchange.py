e0f9fcafcbd7aba3a85b0c39baae364ac93508ba
Matt Gebert <mattheuu.gebert@gmail.com>
2025-07-08 01:54:25 +1000

    test(test_interchange.py): Added test case for conversion of asp_db classes to asf

--- tests/models/test_interchange.py
+++ tests/models/test_interchange.py
@@ -2,10 +2,56 @@
 Tests the conversion between `asf` and `asp` objects.
 """
 
+import numpy as np
+import pytest
+import warnings
+from kkcalc.asf_database import (
+    asp_db_im,
+    asp_db_re,
+    asp_db_im_extended,
+    asp_db_re_extended,
+    asp_db_complex,
+    asp_db_complex_extended,
+)
 
-class TestASFToASP:
+from ..test_stoich import basic_stoichs as bs
+
+
+class TestAsfToAsp:
     """Tests factors to polynomial representation conversion."""
 
 
-class TestASPToASF:
+class TestAspToAsf:
     """Tests polynomial representation to factors conversion."""
+
+
+class TestDbAspToAsf:
+    """Tests database polynomial representation to factors conversion."""
+
+    @pytest.mark.parametrize(
+        "model",
+        [
+            asp_db_im,
+            asp_db_re,
+            asp_db_complex,
+        ],
+    )
+    def test_to_asf(self, model):
+        stoich = bs.POLYMER_P3HT
+        density = 1.33
+        # Create the polynomial object
+        poly = model(stoich)
+        poly.density = density
+        # Convert to asf
+        asf = poly.to_asf()
+        # Check the asf object
+        assert np.isclose(
+            asf.density, density, rtol=1e-5
+        ), "The density should match the input density."
+        assert (
+            asf.stoichiometry == stoich
+        ), "The stoichiometry should match the input stoichiometry."
+        if isinstance(poly, (asp_db_im, asp_db_complex)):
+            assert asf.betas is not None, "The asf object should have betas defined."
+        if isinstance(poly, (asp_db_re, asp_db_complex)):
+            assert asf.deltas is not None, "The asf object should have deltas defined."
